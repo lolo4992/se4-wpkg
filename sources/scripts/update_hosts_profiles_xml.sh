869a81bbbe9c7531d0209af39c7764178343714d
lolo4992 <laurentjoly@orange.fr>
2019-01-11 11:54:54 +0100

    suppression du transferts des infos ldap vers xml

---
+++ sources/scripts/update_hosts_profiles_xml.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#########################################################################
+#   /usr/share/se3/scripts/update_hosts_profiles_xml.sh                 #
+#                                                                       #
+#########################################################################
+#
+#
+#   Met à jour hosts.xml et profiles.xml dans /var/se3/unattended/install/wpkg
+#   à partir des données de l'annuaire
+#
+#   A executer chaque fois que les parcs sont modifiés
+#   Syntaxe :  update_hosts_profiles_xml.sh ComputersRDN ParcsRDN BaseDN
+
+## $Id$ ##
+# last update fev 2016 - utf8
+
+
